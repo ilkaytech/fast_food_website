25295a7e38574b0cc3178149608a2f1ca2cbd8fc
ilkay <06ilkayozturk@gmail.com>
2023-08-06 20:41:45 +0300

    responsive

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,11 +1,11 @@
 import Logo from "../ui/Logo";
+import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+
 const Header = () => {
   return (
     <div className="h-[5.5rem] bg-secondary ">
       <div className="container mx-auto text-white flex justify-between items-center h-full">
-        <div>
-          <Logo />
-        </div>
+        <Logo />
         <nav>
           <ul className="flex gap-x-2">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
@@ -22,7 +22,20 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <div></div>
+        <div className="flex gap-x-4 items-center">
+          <a href="#">
+            <FaUserAlt />
+          </a>
+          <a href="#">
+            <FaShoppingCart />
+          </a>
+          <a href="#">
+            <FaSearch />
+          </a>
+          <a href="#">
+            <button className="btn-primary">Order Online</button>
+          </a>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      container: {
+        screens: {
+          xs: "375px",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
+      },
       colors: {
         primary: "#ffbe33",
         secondary: "#222831",
