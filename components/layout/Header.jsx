44beb1014441a9b9b39f4c573549051f4ce993ad
ilkay <06ilkayozturk@gmail.com>
2023-08-07 00:36:51 +0300

    hamburger

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
-
 import Logo from "../ui/Logo";
 import Search from "../ui/Search";
+import { FaHamburger } from "react-icons/fa";
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false);
+  const [isMenuModal, setIsMenuModal] = useState(false);
   return (
     <div className="h-[5.5rem] bg-secondary ">
       <div className="container mx-auto text-white flex justify-between items-center h-full">
         <Logo />
-        <nav>
-          <ul className="flex gap-x-2">
+        <nav
+          className="sm:static absolute top-0 left-0 grid 
+        place-content-center sm:w-auto sm:h-auto w-full h-full sm:text-white text-black "
+        >
+          <ul className="flex gap-x-2 sm:flex-row flex-col items-center ">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
               <a href="">Home</a>
             </li>
@@ -36,9 +40,12 @@ const Header = () => {
           <button onClick={() => setIsSearchModal(true)}>
             <FaSearch className="hover:text-primary transition-all" />
           </button>
-          <a href="#">
+          <a href="#" className="md:inline-block hidden sm">
             <button className="btn-primary">Order Online</button>
           </a>
+          <button className="sm:hidden inline-block ">
+            <FaHamburger className="text-xl hover:text-primary" />
+          </button>
         </div>
       </div>
       {isSearchModal && <Search setIsSearchModal={setIsSearchModal} />}
