0d207a6b54bfa957736b52daaa7cd2a7d16370aa
ilkay <06ilkayozturk@gmail.com>
2023-08-08 02:09:42 +0300

    commit

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -7,7 +7,12 @@ const Carousel = () => {
     <div className="h-screen w-full container mx-auto -mt-[88px] ">
       <div className="absolute top-0 left-0 w-full h-full">
         <div className="relative h-full w-full ">
-          <Image src="/images/bg1.jpg" alt="" layout="fill" objectFit="cover" />
+          <Image
+            src="/images/bg4.jpeg"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
       </div>
       <div className="relative text-white top-48 flex flex-col items-start gap-y-10">
