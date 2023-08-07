5bac42ec76ed1a95c9c414421bafa2d0a8184ba7
ilkay <06ilkayozturk@gmail.com>
2023-08-08 00:42:07 +0300

    hamburger menu end

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -13,10 +13,9 @@ const Header = () => {
       <div className="container mx-auto text-white flex justify-between items-center h-full">
         <Logo />
         <nav
-          className={`sm:static absolute top-0 left-0 grid 
-        place-content-center sm:w-auto sm:h-auto w-full h-full sm:text-white text-black 
-        sm:bg-transparent bg-white
-        ${isMenuModal !== true && "hidden"}`}
+          className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full
+           sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden
+        ${isMenuModal === true && "!grid place-content-center"}`}
         >
           <ul className="flex gap-x-2 sm:flex-row flex-col items-center ">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
