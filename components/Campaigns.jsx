aa19b8aa6eefc5f64631108d78b0d156e0ae7e41
ilkay <06ilkayozturk@gmail.com>
2023-08-09 22:50:27 +0300

    campaigns end

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Title from "./ui/Title";
+import { LuShoppingCart } from "react-icons/lu";
 
 const CampaignsItem = () => {
   return (
-    <div className="bg-secondary flex-1 rounded-md py-5 px-[15px] ">
+    <div
+      className="bg-secondary flex-1 rounded-md py-5 px-[15px]
+    flex items-center gap-x-4 "
+    >
       <div
-        className="relative w-44 h-44 after:content-[''] border-[5px]
+        className="relative md:w-44 md:h-44 w-36 h-36 after:content-[''] border-[5px]
          border-primary rounded-full overflow-hidden "
       >
         <Image
@@ -15,13 +20,24 @@ const CampaignsItem = () => {
           objectFit="cover"
         />
       </div>
+      <div className="text-white ">
+        <Title addClass="text-2xl">Tasty Thursdays</Title>
+        <div className="font-dancing my-1">
+          <span className="text-[40px] font-dancing ">20%</span>
+          <span className="text-sm inline-block ml-1 ">Off</span>
+        </div>
+        <button className="btn-primary flex items-center gap-x-2 ">
+          {" "}
+          Order Now <LuShoppingCart size={20} />{" "}
+        </button>
+      </div>
     </div>
   );
 };
 
 const Campaigns = () => {
   return (
-    <div className="flex justify-between container mx-auto py-20 gap-5">
+    <div className="flex justify-between container mx-auto py-20 gap-6 flex-wrap">
       <CampaignsItem />
       <CampaignsItem />
     </div>
