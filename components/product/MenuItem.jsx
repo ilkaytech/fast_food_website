89c7a77c72b50ae438bb48150df79facd1cfe964
ilkay <06ilkayozturk@gmail.com>
2023-08-09 23:25:31 +0300

    Menuitem

---
+++ components/product/MenuItem.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+const MenuItem = () => {
+  return (
+    <div>
+      <div className="relative">
+        <Image src="/images/BBQ_Bacon-8045.png" alt="" layout="fill" />
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default MenuItem;
