5551e76e1c3ed40a5d0975ea2a48dca5fdfc75bc
ilkay <06ilkayozturk@gmail.com>
2023-08-10 13:20:12 +0300

    menuitem

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const MenuItem = () => {
   return (
     <div>
-      <div className="relative">
+      <div className="relative w-40 h-40 z-50 ">
         <Image src="/images/BBQ_Bacon-8045.png" alt="" layout="fill" />
       </div>
       <div></div>
