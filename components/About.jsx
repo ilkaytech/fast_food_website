583e8e7bf28b68772bfa036df039ed8690ccafc6
ilkay <06ilkayozturk@gmail.com>
2023-08-10 18:27:44 +0300

    about end

--- components/About.jsx
+++ components/About.jsx
@@ -4,17 +4,24 @@ import Title from "./ui/Title";
 const About = () => {
   return (
     <div className="bg-secondary py-14">
-      <div className="contaner mx-auto">
-        <div className="relative w-[445px] h-[600px]">
-          <Image src="/images/about-img.png" alt="" layout="fill" />
+      <div className="container mx-auto flex items-center text-white gap-20 justify-center flex-wrap-reverse">
+        <div className="flex justify-center">
+          <div className="relative w-[445px] h-[600px] flex justify-center w-[300px] h-[450px]">
+            <Image src="/images/about-img.png" alt="" layout="fill" />
+          </div>
         </div>
-        <div>
-          <Title>We are Feane</Title>
-          <p className="text-white ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            modi odit nobis tenetur vero aspernatur, eveniet rem voluptate
-            veniam error.
+        <div className="md:w-1/2">
+          <Title addClass="text-[40px]">We are Feane</Title>
+          <p className="my-5 flex flex-col items-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            magnam nobis fuga ex quasi animi recusandae debitis iusto laborum,
+            eius quo. Ullam error quo aut? Officiis, doloribus. Modi fugiat ad
+            nostrum autem reiciendis expedita repellendus illum omnis voluptatem
+            provident distinctio quisquam eos corrupti ipsa temporibus
+            perferendis deleniti officia sapiente, magni ut! Accusamus, adipisci
+            quod eveniet facilis eius reprehenderit omnis corrupti!
           </p>
+          <button className="btn-primary">Read More</button>
         </div>
       </div>
     </div>
