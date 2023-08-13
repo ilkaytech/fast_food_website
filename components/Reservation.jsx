90747019c32fb467978a7ff27da6321ee83e74e7
ilkay <06ilkayozturk@gmail.com>
2023-08-13 22:39:03 +0300

    responsive

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -7,7 +7,7 @@ const Reservation = () => {
     <div className="container mx-auto py-12 ">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
       <div className="flex justify-between flex-wrap gap-x-10">
-        <div className="md:flex-1">
+        <div className="lg:flex-1 w-full">
           <div className="flex flex-col gap-y-3">
             <Input />
             <Input />
@@ -17,7 +17,7 @@ const Reservation = () => {
           </div>
           <button className="btn-primary mt-4">Book Now</button>
         </div>
-        <div className="md:flex-1 !h-[384px]">
+        <div className="lg:flex-1">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.7300023882344!2d32.83207298375767!3d39.92505801200679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f190a9cea8f%3A0xd3862ea8248d08a0!2sAn%C4%B1tkabir!5e0!3m2!1str!2str!4v1691789419782!5m2!1str!2str"
             allowFullScreen=""
