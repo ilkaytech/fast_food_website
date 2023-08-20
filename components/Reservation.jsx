70e64d763a51d77bf87e682f981ea6dd85ce642e
ilkay <06ilkayozturk@gmail.com>
2023-08-20 19:25:34 +0300

    edit

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -33,7 +33,7 @@ const Reservation = () => {
   return (
     <div className="container mx-auto py-12 ">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
-      <div className="flex justify-between flex-wrap-reverse gap-x-10">
+      <div className="flex justify-between flex-wrap-reverse gap-10">
         <div className="lg:flex-1 w-full">
           <div className="flex flex-col gap-y-3">
             {inputs.map((input) => (
@@ -44,11 +44,11 @@ const Reservation = () => {
         </div>
         <div className="lg:flex-1 w-full">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.7300023882344!2d32.83207298375767!3d39.92505801200679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f190a9cea8f%3A0xd3862ea8248d08a0!2sAn%C4%B1tkabir!5e0!3m2!1str!2str!4v1691789419782!5m2!1str!2str"
-            allowFullScreen=""
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.729999107405!2d32.834368975589534!3d39.925058085418186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f190a9cea8f%3A0xd3862ea8248d08a0!2sAn%C4%B1tkabir!5e0!3m2!1str!2str!4v1692548579305!5m2!1str!2str"
+            allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
-            className="w-ful h-ful"
+            className="h-full w-full"
           ></iframe>
         </div>
       </div>
