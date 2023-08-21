6e70cd7676c546765d22105ba9ad2c0f6950659f
ilkay <06ilkayozturk@gmail.com>
2023-08-21 20:43:32 +0300

    values

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -1,38 +1,56 @@
 import React from "react";
 import Title from "./ui/Title";
 import Input from "./form/Input";
+import { useFormik } from "formik";
 
 const Reservation = () => {
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      persons: "",
+      date: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   const inputs = [
     {
       id: 1,
       name: "fullName",
       type: "text",
       placeholder: "Your Full Name",
+      value: formik.values.fullName,
     },
     {
       id: 2,
       name: "phoneNumber",
       type: "number",
       placeholder: "Your Phone Number",
+      value: formik.values.phoneNumber,
     },
     {
       id: 3,
       name: "email",
       type: "email",
       placeholder: "Your Email Address",
+      value: formik.values.email,
     },
     {
       id: 4,
       name: "persons",
       type: "number",
       placeholder: "How Many Persons?",
+      value: formik.values.persons,
     },
     {
       id: 5,
-      name: "persons",
+      name: "date",
       type: "datetime-local",
-      placeholder: "How Many Persons?",
+      value: formik.values.date,
     },
   ];
 
@@ -40,14 +58,14 @@ const Reservation = () => {
     <div className="container mx-auto py-12 ">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
       <div className="flex justify-between flex-wrap-reverse gap-10">
-        <div className="lg:flex-1 w-full">
+        <form className="lg:flex-1 w-full" onSubmit={formik.handleSubmit}>
           <div className="flex flex-col gap-y-3">
             {inputs.map((input) => (
-              <Input key={input.id} {...input} />
+              <Input key={input.id} {...input} onChange={formik.handleChange} />
             ))}
           </div>
           <button className="btn-primary mt-4">Book Now</button>
-        </div>
+        </form>
         <div className="lg:flex-1 w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.729999107405!2d32.834368975589534!3d39.925058085418186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f190a9cea8f%3A0xd3862ea8248d08a0!2sAn%C4%B1tkabir!5e0!3m2!1str!2str!4v1692548579305!5m2!1str!2str"
