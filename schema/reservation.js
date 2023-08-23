9f6f9aecd888a551357b7cb7ec9d9c577221eb79
ilkay <06ilkayozturk@gmail.com>
2023-08-23 23:39:19 +0300

    commit yup

--- schema/reservation.js
+++ schema/reservation.js
@@ -4,5 +4,9 @@ export const reservationSchema = Yup.object({
   fulName: Yup.string()
     .required("Full name is required")
     .min(3, "Full name must be at least 3 characters"),
-  phoneNumber: Yup.string(),
+  phoneNumber: Yup.string().min(
+    10,
+    "Phone number must be at least 10 chracters"
+  ),
+  email: Yup.string().email("Email is invalid"),
 });
