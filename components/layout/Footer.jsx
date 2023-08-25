159ea9f0c7580b2259f95abf9bdd30c28cc1777b
ilkay <06ilkayozturk@gmail.com>
2023-08-25 23:39:26 +0300

    footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Title from "../ui/Title";
 
 const Footer = () => {
   return (
-    <div className="bg-secondary">
+    <div className="bg-secondary text-white">
       <div className="container mx-auto">
-        <div></div>
         <div>
-          <p>@ 2023 All Rights Reserved ilkaytech</p>
+          <div>
+            <Title addClass="text-[30px]">Contact Us</Title>
+            <div>
+              <span></span>
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <p className="text-center">@ 2023 All Rights Reserved ilkaytech</p>
         </div>
       </div>
     </div>
