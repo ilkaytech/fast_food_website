fbe364e183b80c31db58755dc77259331139d2c6
ilkay <06ilkayozturk@gmail.com>
2023-08-25 15:11:23 +0300

    customers_onclick

--- components/costomers/CustomerItem.jsx
+++ components/costomers/CustomerItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CustomerItem = ({ imgSrc }) => {
   return (
-    <div className="mt-5">
+    <div className="mt-5 mx-4">
       <div className="p-6 bg-secondary text-white rounded-[5px] ">
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. At nulla
