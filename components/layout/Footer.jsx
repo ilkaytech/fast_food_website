6b87ca6be3bad67345df473eacca821ab379f518
ilkay <06ilkayozturk@gmail.com>
2023-08-26 13:32:43 +0300

    footer_fontawesome

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -4,19 +4,64 @@ import Title from "../ui/Title";
 const Footer = () => {
   return (
     <div className="bg-secondary text-white">
-      <div className="container mx-auto">
-        <div>
+      <div className="container mx-auto pt-16 pb-6">
+        <div className="flex justify-between text-center ">
           <div>
             <Title addClass="text-[30px]">Contact Us</Title>
-            <div>
-              <span></span>
+            <div className="flex flex-col gap-y-2 mt-6">
+              <div>
+                <i className="fa-solid fa-location-dot"></i>
+                <span className="inline-block ml-2">Location</span>
+              </div>
+              <div>
+                <i className="fa fa-phone"></i>
+                <span className="inline-block ml-2">Call +1 530 264 82 14</span>
+              </div>
+              <div>
+                <i className="fa fa-envelope"></i>
+                <span className="inline-block ml-2">info@ilkaytech.com</span>
+              </div>
             </div>
           </div>
-          <div></div>
-          <div></div>
+          <div>
+            <Title addClass="text-[30px]">Feane</Title>
+            <div className="flex flex-col gap-y-2 mt-6">
+              <div>
+                <i className="fa-solid fa-location-dot"></i>
+                <span className="inline-block ml-2">Location</span>
+              </div>
+              <div>
+                <i className="fa fa-phone"></i>
+                <span className="inline-block ml-2">Call +1 530 264 82 14</span>
+              </div>
+              <div>
+                <i className="fa fa-envelope"></i>
+                <span className="inline-block ml-2">info@ilkaytech.com</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Title addClass="text-[30px]">Opening Hours</Title>
+            <div className="flex flex-col gap-y-2 mt-6">
+              <div>
+                <i className="fa-solid fa-location-dot"></i>
+                <span className="inline-block ml-2">Location</span>
+              </div>
+              <div>
+                <i className="fa fa-phone"></i>
+                <span className="inline-block ml-2">Call +1 530 264 82 14</span>
+              </div>
+              <div>
+                <i className="fa fa-envelope"></i>
+                <span className="inline-block ml-2">info@ilkaytech.com</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div>
-          <p className="text-center">@ 2023 All Rights Reserved ilkaytech</p>
+          <p className="text-center mt-10">
+            @ 2023 All Rights Reserved ilkaytech
+          </p>
         </div>
       </div>
     </div>
--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,14 @@ export default function Document() {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+        {/* Font Awesome CDN */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
       </Head>
       <body>
         <Main />
