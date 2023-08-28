0970715d8773eab470f68c1caa47f7521f7e9b59
ilkay <06ilkayozturk@gmail.com>
2023-08-29 00:16:01 +0300

    commit

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -3,17 +3,15 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div className="">
-      <div>
-        <div>
-          <div className="relative">
-            <Image src="/images/client2.jpg" alt="" width={60} height={60} />
-            <b>John Doe</b>
-          </div>
-          <div></div>
+    <div className="flex">
+      <div className="border p-10">
+        <div className="relative">
+          <Image src="/images/client2.jpg" alt="" width={60} height={60} />
+          <b>John Doe</b>
         </div>
         <div></div>
       </div>
+      <div></div>
     </div>
   );
 };
