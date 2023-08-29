c4f045f1da60e39dc9b00a0944ebb4b86e238114
ilkay <06ilkayozturk@gmail.com>
2023-08-29 23:38:07 +0300

    profile_button

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const Profile = () => {
   return (
     <div className="flex px-10">
-      <div className="border w-77">
-        <div className="relative flex flex-col items-center p-10">
+      <div className="border w-80">
+        <div className="relative flex flex-col items-center px-10 py-5">
           <Image
             src="/images/client2.jpg"
             alt=""
@@ -17,20 +17,32 @@ const Profile = () => {
         </div>
         <ul className="">
           <li
-            className="border w-full p-2 cursor-pointer hover:bg-primary
+            className="border w-full p-3 cursor-pointer hover:bg-primary
           hover:text-white transition-all  "
           >
-            <i className="fa fa-home"></i>
+            <i class="fas fa-house-user"></i>
             <button className="ml-1">Account</button>
           </li>
-          <li>
-            <button>Password</button>
+          <li
+            className="border w-full p-3 cursor-pointer hover:bg-primary
+          hover:text-white transition-all  "
+          >
+            <i class="fas fa-key"></i>
+            <button className="ml-1">Password</button>
           </li>
-          <li>
-            <button>Orders</button>
+          <li
+            className="border w-full p-3 cursor-pointer hover:bg-primary
+          hover:text-white transition-all  "
+          >
+            <i class="fas fa-shipping-fast"></i>
+            <button className="ml-1">Orders</button>
           </li>
-          <li>
-            <button>Çıkış</button>
+          <li
+            className="border w-full p-3 cursor-pointer hover:bg-primary
+          hover:text-white transition-all  "
+          >
+            <i class="fas fa-sign-out-alt"></i>
+            <button className="ml-1">Exit</button>
           </li>
         </ul>
       </div>
