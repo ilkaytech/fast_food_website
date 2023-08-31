7521f59b1e6c86c7b8466a8eab6209b4595c9600
ilkay <06ilkayozturk@gmail.com>
2023-08-31 16:55:44 +0300

    admin_products

--- pages/admin/profile.jsx
+++ pages/admin/profile.jsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Account from "../../components/profile/Account";
 import Password from "../../components/profile/Password";
-import Order from "../../components/profile/Order";
+
+import Products from "../../components/admin/Products";
 
 const Profile = () => {
   const [tabs, setTabs] = useState(0);
@@ -18,13 +19,13 @@ const Profile = () => {
         border border-b-0"
         >
           <Image
-            src="/images/client2.jpg"
+            src="/images/admin.png"
             alt=""
             width={100}
             height={100}
             className="rounded-full"
           />
-          <b className="text-2xl mt-1">John Doe</b>
+          <b className="text-2xl mt-1">Admin</b>
         </div>
         <ul className=" text-center font-semibold">
           <li
@@ -34,8 +35,8 @@ const Profile = () => {
           } `}
             onClick={() => setTabs(0)}
           >
-            <i class="fas fa-house-user"></i>
-            <button className="ml-2">Account</button>
+            <i class="fas fa-cutlery"></i>
+            <button className="ml-2">Products</button>
           </li>
           <li
             className={`border w-full p-3 cursor-pointer hover:bg-primary
@@ -44,8 +45,8 @@ const Profile = () => {
             } `}
             onClick={() => setTabs(1)}
           >
-            <i class="fas fa-key"></i>
-            <button className="ml-2">Password</button>
+            <i class="fas fa-shipping-fast"></i>
+            <button className="ml-2">Orders</button>
           </li>
           <li
             className={`border w-full p-3 cursor-pointer hover:bg-primary
@@ -54,8 +55,8 @@ const Profile = () => {
             } `}
             onClick={() => setTabs(2)}
           >
-            <i class="fas fa-shipping-fast"></i>
-            <button className="ml-2">Orders</button>
+            <i class="fas fa-key"></i>
+            <button className="ml-2">Categories</button>
           </li>
           <li
             className={`border w-full p-3 cursor-pointer hover:bg-primary
@@ -64,12 +65,22 @@ const Profile = () => {
             } `}
             onClick={() => setTabs(3)}
           >
+            <i class="fas fa-key"></i>
+            <button className="ml-2">Footer</button>
+          </li>
+          <li
+            className={`border w-full p-3 cursor-pointer hover:bg-primary
+            hover:text-white transition-all ${
+              tabs === 4 && "bg-primary text-white"
+            } `}
+            onClick={() => setTabs(4)}
+          >
             <i class="fas fa-sign-out-alt"></i>
             <button className="ml-2">Exit</button>
           </li>
         </ul>
       </div>
-      {tabs === 0 && <Account />}
+      {tabs === 0 && <Products />}
       {tabs === 1 && <Password />}
     </div>
   );
