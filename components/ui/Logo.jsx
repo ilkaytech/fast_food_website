ea1ec61b8fbb3b4032790e305af39e6fadab7c31
ilkay <06ilkayozturk@gmail.com>
2023-09-06 17:42:29 +0300

    logo

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -4,10 +4,9 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/">
-      <img src="./images/dd.png" alt="" className="w-32 h-66 " />
-      {/* <span className="text-[2rem] font-dancing font-bold cursor-pointer ">
+      <span className="text-[2rem] font-dancing font-bold cursor-pointer ">
         Feane
-      </span> */}
+      </span>
     </Link>
   );
 };
