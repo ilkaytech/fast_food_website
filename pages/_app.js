0ee3beeb1e0fe5559a757b380ffc4e774673f633
ilkay <06ilkayozturk@gmail.com>
2023-09-09 15:29:25 +0300

    redux_cartSlice

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,17 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/globals.css";
 import Layout from "../layout/Layout";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
+
+export default MyApp;
